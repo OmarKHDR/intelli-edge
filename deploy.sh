e5a9ee8a9e11abfa92b3eae81991efe523c14b11
OmarKHDR <omarsliman37@gmail.com>
2025-12-03 21:32:14 +0200

    master edits

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ set -e
 
 # 1. Build the Next.js static site
 npm run build
-npx next export
 
 # 2. Remove everything in current directory except `out` and `CNAME`
 find . -mindepth 1 -maxdepth 1 ! -name 'out' ! -name 'CNAME' ! -name 'deploy.sh' -exec rm -rf {} +
